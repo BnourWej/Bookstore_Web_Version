1b38e5d9ba71c4597f7f427e9bd8d1ba5a4f8121
wejdeneBannour <bnwejdene@gmail.com>
2020-12-05 00:35:31 +0100

    adding order entity

--- src/main/java/com/bookstore/Repository/BookRepository.java
+++ src/main/java/com/bookstore/Repository/BookRepository.java
@@ -2,7 +2,7 @@ package com.bookstore.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.bookstore.model.Book;
+import com.bookstore.entities.Book;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
